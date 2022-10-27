fc8ba418b64e621d8deb4b5cb6fc44c0ab4b6073
Or <orrodrigez1@gmail.com>
2022-10-27 21:53:19 +0300

    Make a multiply function

--- scripts.js
+++ scripts.js
@@ -9,4 +9,10 @@ function subtract([...args]) {
   const sum = args.reduce((total, next) => total - next)
   return sum;
 }
+
+function multiply([...args]) {
+  const sum = args.reduce((total, next) => total * next)
+  return sum;
+}
+
 //end calculation functions
