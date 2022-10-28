5f95ee42ff292db39263090c6329317edbd68a25
Or <orrodrigez1@gmail.com>
2022-10-28 09:26:37 +0300

    Make an operate function Make an operate function to call the proper calculation function.

--- scripts.js
+++ scripts.js
@@ -22,3 +22,17 @@ function divide([...args]) {
   return sum.toFixed(2);
 }
 //end calculation functions
+
+//operate
+function operate(a, operator, b) {
+  if (operator === "+") {
+    return add([a, b])
+  } else if (operator === "-") {
+    return subtract([a, b])
+  } else if (operator === "*") {
+    return multiply([a, b]) 
+  } else if (operator === "/") {
+    return divide([a, b])
+  }
+}
+//operate
